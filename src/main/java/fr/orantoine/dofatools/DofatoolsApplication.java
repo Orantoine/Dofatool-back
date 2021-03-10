a9048849a3f1c9a93cdb235e67a5b61325dc2bbe
Antoine GODEAU <inazuma80120@gmail.com>
2021-03-10 23:07:43 +0100

    First Commit

---
+++ src/main/java/fr/orantoine/dofatools/DofatoolsApplication.java
@@ -0,0 +1,13 @@
+package fr.orantoine.dofatools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofatoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DofatoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/orantoine/dofatools/DofatoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orantoine.dofatools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DofatoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
